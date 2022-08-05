6bba0bbf5c5861e55506782c381380d9457b63e7
letnagao <leticia-nagao@hotmail.com>
2022-08-04 23:43:43 -0300

    added enemies class

--- js/script.js
+++ js/script.js
@@ -1,15 +1,41 @@
 const $ = (tag) => document.querySelector(tag)
 
 const cnv = $('canvas')
-cnv.width = innerWidth
-cnv.height = innerHeight
-
+    cnv.width = innerWidth
+    cnv.height = innerHeight
 const ctx = cnv.getContext('2d')
-
 const player = new Player(cnv.width / 2, cnv.height / 2, 30, '#48FCFF')
+const shootingSpeed = 4
 
 let projectiles = []
-const shootingSpeed = 4
+let enemies = []
+let intervalID 
+
+function spawnEnemies() {
+    intervalID = setInterval(() => {
+        const radius = Math.floor(Math.random() * 26) + 5
+
+        let posX, posY
+        if(Math.random() < .5) {
+            posX = Math.random() < .5 ? 0 - radius : cnv.width + radius
+            posY = Math.random() * cnv.height
+        } else {
+            posX = Math.random() * cnv.width
+            posY = Math.random() < .5 ? 0 - radius : cnv.height + radius
+        }
+
+        const angle = Math.atan2(player.y - posY, player.x - posX)
+
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+        const color = 'hsl('+ Math.random() * 360 +',50%,50%)'
+
+        enemies.push(new Enemy(posX,posY,radius,color,velocity))
+    },1500)
+}
 
 cnv.addEventListener('click', (e) => {
     e.preventDefault()
@@ -30,8 +56,15 @@ function update() {
     ctx.fillStyle = 'rgba(0, 0, 0, .1)'
     ctx.fillRect(0, 0, cnv.width, cnv.height)
 
+    checkEnemies()
     checkProjectiles()
     player.update()
+}
+
+function checkEnemies() {
+    enemies.forEach((enemy) => {
+        enemy.update()
+    }) 
 }
 
 function checkProjectiles() {
@@ -53,3 +86,4 @@ function checkOffScreen(projectile, index) {
 }
 
 loop()
+spawnEnemies()
